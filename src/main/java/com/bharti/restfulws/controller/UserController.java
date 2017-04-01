f3c579efaef44cc1b4963ef59a02ce2760df67a4
amit-bharti <amitb.mca@gmail.com>
2017-04-01 23:03:17 +0530

    Modified files for Restful Changes

--- src/main/java/com/bharti/restfulws/controller/UserController.java
+++ src/main/java/com/bharti/restfulws/controller/UserController.java
@@ -3,20 +3,62 @@
  */
 package com.bharti.restfulws.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bharti.restfulws.domain.User;
+import com.bharti.restfulws.presentation.domain.UserList;
 
 /**
  * @author AMIT
  *
  */
-@Controller
 @RequestMapping("/user")
+@RestController("userController")
 public class UserController {
 		
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public void getUserById() {
+	@RequestMapping(value="/{id}", method=RequestMethod.GET, headers="Accept=text/html,application/json")
+	public User getUserById(@PathVariable long id) {		
+		return new User();
+	}
+	
+	@RequestMapping(value="/", method=RequestMethod.GET, produces={"application/json","application/xml"})	
+	public UserList getAllUsers() {		
+		return prepareList();
+	}
+	
+	/*@RequestMapping(value="/", method=RequestMethod.GET)
+	public String test(Model model) {		
+		return "index";
+	}*/
+	
+	/*@RequestMapping(value="/", method=RequestMethod.GET)	
+	public @ResponseBody User getUser() {		
+		User u = new User();
+		u.setFirstName("Amit");
+		u.setLastName("Bharti");
+		return u;
+	}*/
+	
+	private UserList prepareList() {
+		List<User> l = new ArrayList<User>(); 
 		
+		User u1 = new User();
+		u1.setFirstName("Amit");
+		u1.setLastName("Bharti");
+		
+		User u2 = new User();
+		u2.setFirstName("Neelima");
+		u2.setLastName("Bharti");
+		
+		l.add(u1);
+		l.add(u2);
+		
+		return new UserList(l.size(), l);
 	}
 }
